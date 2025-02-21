4013a64b35a923a8920b6806afc81c6dfba6e287
saqlain Mushtaq <saqlainmushtaq756@gmail.com>
2025-02-21 12:46:46 +0500

    01-DataType (Primtive)

---
+++ DataType.js
@@ -0,0 +1,41 @@
+// JavaScript Notes
+
+// 1. DataTypes
+
+// hamray pas 2 trah kay DataTypes hotay hain aik hota hay
+// 1. Primtive Data Type
+// 2. Reference type (non Primtive )
+
+// Primtive data type wo hotay hain nokay as it values ko use krnay kay lye use hotay hain.
+// hmaray pas 7 trah kay primtive data types hotay hain
+// string, bolean, null , symbel, bigint , number,undefined
+
+// 2. Reference Type : ye hmaray pas 3 trah kay hotay hain (array , object, functions)
+// esmain hmain data ka reference mlta hay
+
+// know we start out DataType
+
+// ====> Primtive DataType:
+// 1  string data type
+let myName = "saqlain";
+// console.log("name:", myName);
+// 2  number type
+let numers = 14782;
+// ==>3 undefined
+let number = undefined; // ye empty value deta hay for the future use
+
+let bigNumber = 1478520n; // ye numbers ko bigint main change krta hya hmaray pas jo values hoti hain unkay end main ham "n" ka use
+// krtay hain
+
+// let null
+
+let value = null; // ye hmain empty return krta hy
+
+let loginId = false; // ye hmain btati hay kay value bolean
+
+let symberValue = Symbol("14785"); // ye hmain btati hay kay hmari value unique hay aur ham essay future main use
+// kar saktay hain
+
+// know we discus about reference type or  non-Primtive values
+// we have 3 dataTypes Array,object and function
+// Arrays
