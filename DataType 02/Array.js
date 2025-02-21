bacc5dfdeba273a8a02131f75d051a6fb9fc8f52
saqlain Mushtaq <saqlainmushtaq756@gmail.com>
2025-02-21 13:08:27 +0500

    DataType(non_Primtive)

---
+++ DataType 02/Array.js
@@ -0,0 +1,8 @@
+// hmaray pas array aik DataType hy jokay reference type bhi kehlati hay
+// hmaray pas arra ko lekhnay ka tareka es trah hota hay ham sabsay pehlay sqare braket ko aik variable main
+// store krwata hay hmaray elements mokhtalef trah kay hosaktay hain mtlab hmaray elements nubmer,string null kesi bhi type kay hosktay hain
+let Array = [1, 2, 3, 4, 5, 6, 9, "saqlian", undefined, null];
+console.log("array is", Array);
+
+// array hmesha values ko 0 index say store krwata hy
+console.log("array is", Array[6]);
